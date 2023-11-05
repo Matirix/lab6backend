cca083426d4fd2deca346949c13ca998429220b3
Matthew <Matirix@live.com>
2023-11-04 19:05:37 -0700

    work

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use((req, res, next) => {
     requestCounter++;
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     pool.getConnection((err, connection) => {
         if (err) {

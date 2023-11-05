3ceca743dbf2b20bc314c38fe58e36700b602277
Matthew <Matirix@live.com>
2023-11-04 19:03:38 -0700

    work

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ let requestCounter = 0;
 
 const app = express();
 app.use(express.json());
+res.header('Access-Control-Allow-Origin', '*');
+res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 app.use((req, res, next) => {
     requestCounter++;
-    res.header(defaultHeader);
     pool.getConnection((err, connection) => {
         if (err) {
             console.error('Error getting a MySQL connection:', err);
